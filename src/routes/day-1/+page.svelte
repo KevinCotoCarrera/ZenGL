7d3753ab64eb44e396435992add7b05ffb35e18c
Kevin Coto <kevincoto2002@gmail.com>
2025-02-28 16:34:10 +0700

    fix: enable sound helper text

--- src/routes/day-1/+page.svelte
+++ src/routes/day-1/+page.svelte
@@ -2,11 +2,7 @@
     import { Canvas } from '@threlte/core'
     import Scene from './Scene.svelte';
 	import Phrase from '$lib/components/Phrase/Phrase.svelte';
-    let  disabledBtn = $state(false)
-    function enableSound(){
-      //sound is enabled after clicking the screen
-      disabledBtn = true
-    }
+    
   </script>
   <Phrase
   phrase="目の前に立ちはだかる高い、高い壁"
@@ -14,11 +10,7 @@
   translation="A tall, tall wall looms over me"
   />
   <div class="flex flex-col justify center items-center h-screen w-auto py-24">
-    <button disabled={disabledBtn} onclick={enableSound} class="bg-gray-800 disabled:bg-gray-400 text-white  text-xl w-64 rounded-3xl shadow-2xl px-12 py-4"> Enable Sound</button>
-    {#if disabledBtn}
-    <p class="text-gray-600 text-center mt-4">Audio is enabled</p>
-
-    {/if}
+  <p>Click the Screen to enable Sound</p>
     <Canvas>
     <Scene />
   </Canvas>
