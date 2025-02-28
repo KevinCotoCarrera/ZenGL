19e7161a0374593d85ea01b373f847c41ad95340
Kevin Coto <kevincoto2002@gmail.com>
2025-02-28 16:43:43 +0700

    fix: mixString fn-adding now an ending point

--- src/lib/utils/mixString.ts
+++ src/lib/utils/mixString.ts
@@ -1,7 +1,8 @@
 export function mixString(separator: string, ...args: any[]) {
     let mixString = '';
     for (let i = 0; i < args.length; i++) {
-        mixString += `${args[i]} ${separator} `;
+        
+        mixString += `${args[i]} ${i !== args.length - 1 ? separator: '.'} `;
     }
     return mixString;
 }
